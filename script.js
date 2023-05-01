808d83aae5bbd0d9aa6666e54d4870e1827e01f9
MarwanZahran25 <marwanzahran07@gmail.com>
2023-05-01 03:35:02 +0300

    done

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
+playerScore=0;
+computerScore=0;
 function getPlayerChoice(){
-    let playerChoice = prompt("please choose Rock,paper os scissors");
-    return playerChoice.toUpperCase;
+    let playerChoice = prompt("please choose Rock,paper or scissors");
+    return playerChoice.toUpperCase();
 }
 function getComputerChoice(){
    let computerChoice;
-    let theRandomNumber = (math.random()*10)%3;
+    let theRandomNumber = (Math.floor(Math.random()*10))%3;
    switch (theRandomNumber){
         case 0 :
-            computerChoice = ROCK;
+            computerChoice = "ROCK";
             break;
         case 1 :
-            computerChoice = PAPER;
+            computerChoice = "PAPER";
             break;
         case 2 :
-            computerChoice = SCISSORS;
+            computerChoice = "SCISSORS";
             break;
    }
    return computerChoice;
@@ -25,26 +27,60 @@ function playAround (computerSelection,playerSelection){
         return "DRAW"
     }
     else if (computerSelection == "ROCK" && playerSelection=="SCISSORS"){
+       computerScore++
         return "COMPUTER"
+        
     }
     else if (computerSelection == "ROCK" && playerSelection=="PAPER"){
+       playerScore++;
         return "PLAYER"
+        
+        
     }
     else if (computerSelection == "PAPER" && playerSelection=="ROCK"){
+       computerScore++;
         return "COMPUTER"
+        
     }
     else if (computerSelection == "PAPER" && playerSelection=="SCISSORS"){
+       playerScore++;
         return "PLAYER"
+        
     }
     else if (computerSelection == "SCISSORS" && playerSelection=="ROCK"){
+        playerScore++;
         return "PLAYER"
+        
     }
     else if ((computerSelection == "SCISSORS" && playerSelection=="PAPER")){
+        computerScore++;
         return "COMPUTER"
+        
     }
+}
+function game(){
 
+    let playerChoice = getPlayerChoice();
+    
 
+    playAround(getComputerChoice(),playerChoice);
+    playAround(getComputerChoice(),playerChoice);
+    playAround(getComputerChoice(),playerChoice);
+    playAround(getComputerChoice(),playerChoice);
+    playAround(getComputerChoice(),playerChoice);
+  
 
 }
-playAround("PAPER","PAPER")
-console.log(playAround("PAPER","PAPER"))
+
+game();
+
+
+if (playerScore==computerScore){
+        console.log(`draw`)
+    }
+else if (playerScore>computerScore){
+        console.log(`player wins with score of  ${playerScore} aganist computers ${computerScore}`)
+    }
+else if (playerScore<computerScore){
+        console.log(`computer wins with score of ${computerScore} aganist player's ${playerScore}`)
+    }
